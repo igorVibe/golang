fb04216b6a2b9ad447bc39e76003b6f5b2c9a565
SilvaVibe <igor.juan108@gmail.com>
2025-03-28 08:20:46 -0300

    operador lógico

--- main.go
+++ main.go
@@ -9,4 +9,12 @@ func main() {
 	fmt.Println(" A Divisão é: ", a / b)
 	fmt.Println(" A Multiplicação é: ", a * b)
 	fmt.Println(" O Resto da Divisão é: ", a % b)
+
+	a+=1
+	fmt.Println("Incrementar a", a)
+
+	if a > 0 && b > 0 {
+		fmt.Println("Numeros Positivos")
 }	
+
+	}
